fcf867069e092a265283f6608ea0c2e6e4d4dba5
jhanavi11jhanavi <vadajhanavi@gmail.com>
2026-04-09 12:51:44 +0530

    Lambda functions and list comprehension

---
+++ day.14.py
@@ -0,0 +1,42 @@
+'''
+lambda function():-
+-----------------
+--> this is also called as anonymus function..
+--> A lambda function can take n number of argument but only one expression
+
+synatax
+-------
+lambda(keyword) argument : expression
+
+any = lambda i : i * 19
+print(any(11))
+
+what = lambda am, she : she * am
+print(what(8,19))
+
+go = lambda the, get : get + the
+print(go(22,165))
+
+janu = lambda to, they : they % to
+print(janu(4,2))
+
+there = lambda he, his  : his - he
+print(there(4,64))
+
+list comprehension:
+------------------
+--> This is offers the shorter syntax when you want to create a new list from the existing list
+
+syntax
+------
+variable_name = [expression loop and condition]
+'''
+first_nums = [12,5,34,68,67]
+second_nums = [i for i in first_nums if i%2==0]
+print(second_nums)
+
+
+
+
+
+

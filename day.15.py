384f7d312930c810d493cfeceaf881742b2b228d
jhanavi11jhanavi <vadajhanavi@gmail.com>
2026-04-13 13:07:39 +0530

    Generators

---
+++ day.15.py
@@ -0,0 +1,45 @@
+'''
+Generators
+----------
+this is a special type of functions that return an ITERATOR which one at a time
+
+yield:-
+--> It will take a pause and again resume,this is not a normal keyword cann't be used in the normal functions
+--> this is used to produce a value and pause execution.
+
+next():-
+-----
+--> this is used to get next value from a generators
+--> when the value is finished,it will stop the iterator
+
+def generator():
+    yield 11
+    yield 24
+    yield 19
+    yield 11
+are = generator()
+print(next(are))
+print(next(are))
+print(next(are))
+print(next(are))
+
+def square(n):
+    for i in range(n):
+        yield i * i
+for val in square(7):
+    print(val)
+'''
+def add(n):
+    for i in range(n):
+        yield i + 10
+for val in add(50):
+    print(val)
+
+
+
+
+
+
+
+
+    

7eefd4cc364f1d9dac7406d4be71dbd11743e41f
jhanavi11jhanavi <vadajhanavi@gmail.com>
2026-04-18 12:43:18 +0530

    constructor(__init__)

---
+++ day19.py
@@ -0,0 +1,50 @@
+'''
+constructor(__init__):-
+---------------------
+-->A constructor is a special method used to initiallize object data
+__init__()
+
+class employee:
+    def __init__ (self, name, ID):
+        self.name = name
+        self.ID = ID
+
+    def display(self):
+        print(self.name, self.ID)
+emp_1 = employee("janu",119)
+emp_1.display()
+
+Access Specifiers:-
+-----------------
+1.Public
+2.Protected
+3.Private
+
+1.Public:-
+  ------
+SYNTAX:- name
+we can use it anywhere in a program
+
+2.Protected:-
+  ---------
+SYNTAX:- _name
+This is only for internal use
+
+3.Private:-
+  -------
+SYNTAX:- __name
+This one is restricted
+'''
+class some:
+    def __init__(self):
+        self.public = "janu"
+        self.protected = "jhanavi"
+        self.private = "janujhanavi"
+
+any = some()
+print(any.public)
+print(any._protected)
+print(any.__private)
+
+
+  
